db5e490b35238a7b63314a623f9076ecbec1d115
Lyuflora <378222467@qq.com>
2019-11-28 12:39:49 +0800

    增加手臂动画
    单击鼠标左键挥动右手臂

---
+++ Assets/arm_model/animController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animController : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // anim.SetBool ("slash", Input.getButton ("Fire1"));
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetBool("Hit01", true);
+        }
+        else
+        {
+            anim.SetBool("Hit01", false);
+        }
+    }
+}
